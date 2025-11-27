169b669582544aaa7f6022b1332965ab9f761fa2
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2025-11-27 15:33:44 +0530

    sent a response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,11 +9,15 @@ fn main() {
     // Uncomment the code below to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+
+                let response = format!("+PONG\r\n");
+
+                _stream.write_all(response.as_bytes()).expect("failed to send the data");
             }
             Err(e) => {
                 println!("error: {}", e);
