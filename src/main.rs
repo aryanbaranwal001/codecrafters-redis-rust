f7e072062a13f61f179ec9356d28339181daccd8
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2025-11-28 15:04:39 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
 use std::io::{ Write, Read };
+use std::thread;
 
 use bytes::buf;
 
@@ -9,39 +10,47 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment the code below to pass the first stage
-    //
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for result_stream in listener.incoming() {
-        match result_stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
 
-                let mut buffer = [0; 1024];
-
-                loop {
-                    let bytes_read = stream.read(&mut buffer).unwrap();
-
-                    let received = String::from_utf8_lossy(&buffer[..bytes_read])
-                        .trim()
-                        .to_owned();
-
-                    println!("This is what is received {:?}", received);
-
-                    if received == "*1\r\n$4\r\nPING" {
-                        let response = format!("+PONG\r\n");
-
-                        stream.write_all(response.as_bytes()).expect("failed to send the data");
-                    } else {
-                        stream
-                            .write_all("Go Fuck Yourself".as_bytes())
-                            .expect("failed to send the data");
+        let _ = thread::spawn(|| {
+            
+                match result_stream {
+                    Ok(mut stream) => {
+                        println!("accepted new connection");
+        
+                        let mut buffer = [0; 512];
+        
+                        loop {
+                            let bytes_read = stream.read(&mut buffer).unwrap();
+        
+                            let received = String::from_utf8_lossy(&buffer[..bytes_read])
+                                .trim()
+                                .to_owned();
+        
+                            println!("This is what is received {:?}", received);
+        
+                            if received == "*1\r\n$4\r\nPING" {
+                                let response = format!("+PONG\r\n");
+        
+                                stream.write_all(response.as_bytes()).expect("failed to send the data");
+                            } else {
+                                stream
+                                    .write_all("Go Fuck Yourself\n".as_bytes())
+                                    .expect("failed to send the data");
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("error: {}", e);
                     }
                 }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            });
         }
-    }
+
 }
+
+
+
