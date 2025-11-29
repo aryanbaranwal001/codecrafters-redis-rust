47191d918bd5514867bb4ceccb90a98aa38910da
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2025-11-29 19:08:55 +0530

    codecrafters submit [skip ci]

---
+++ commands.md
@@ -0,0 +1,5 @@
+
+
+`echo -e '*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n' | nc localhost 6379`
+
+`echo -e '*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n' | nc localhost 6379`
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ use std::thread;
 use std::str;
 use tracing::{ info, warn, error };
 use tracing_subscriber;
-
+use tracing_subscriber::fmt::format;
+use std::collections::HashMap;
+use std::sync::{ Arc, Mutex };
 use bytes::buf;
+
+type SharedStore = Arc<Mutex<HashMap<String, String>>>;
 
 fn main() {
     tracing_subscriber::fmt::init();
+    let store: SharedStore = Arc::new(Mutex::new(HashMap::new()));
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -19,7 +24,9 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for result_stream in listener.incoming() {
-        let _ = thread::spawn(|| {
+        let store_clone = store.clone();
+
+        let _ = thread::spawn(move || {
             match result_stream {
                 Ok(mut stream) => {
                     println!("accepted new connection");
@@ -33,9 +40,13 @@ fn main() {
                             .trim()
                             .to_owned();
 
-                        info!("received string ==> {:?}", received);
+                        println!("received string ==> {:?}", received);
 
-                        stream = parse_and_operate(&received, stream);
+                        if received == "" {
+                            break;
+                        }
+
+                        stream = parse_and_operate(&received, stream, &store_clone);
                     }
                 }
                 Err(e) => {
@@ -46,7 +57,9 @@ fn main() {
     }
 }
 
-fn parse_and_operate(received: &String, mut stream: TcpStream) -> TcpStream {
+fn parse_and_operate(received: &String, mut stream: TcpStream, store: &SharedStore) -> TcpStream {
+    let mut map = store.lock().unwrap();
+
     let mut counter = 0;
 
     let bytes_received = received.as_bytes();
@@ -56,7 +69,7 @@ fn parse_and_operate(received: &String, mut stream: TcpStream) -> TcpStream {
         let no_of_elements = bytes_received[counter] - b'0';
         counter += 3;
 
-        info!("no of elements in resp array ==> {}", no_of_elements);
+        println!("no of elements in resp array ==> {}", no_of_elements);
 
         let mut elements_array: Vec<String> = Vec::new();
 
@@ -82,18 +95,34 @@ fn parse_and_operate(received: &String, mut stream: TcpStream) -> TcpStream {
             }
         }
 
-        
-        if elements_array[0].eq_ignore_ascii_case("echo") {
+        println!("elements array after for loop ==> {:?}", elements_array);
 
-            info!("elements array ==> {:?}", elements_array);
-            let bulk_str_to_send = format!("${}\r\n{}\r\n", elements_array[1].as_bytes().len(), elements_array[1]);
+        if elements_array[0].eq_ignore_ascii_case("echo") {
+            println!("elements array ==> {:?}", elements_array);
+            let bulk_str_to_send = format!(
+                "${}\r\n{}\r\n",
+                elements_array[1].as_bytes().len(),
+                elements_array[1]
+            );
             let _ = stream.write_all(bulk_str_to_send.as_bytes());
-        
         } else if elements_array[0].eq_ignore_ascii_case("ping") {
-        
             let _ = stream.write_all("+PONG\r\n".as_bytes());
-        
+        } else if elements_array[0].eq_ignore_ascii_case("set") {
+            map.insert(elements_array[1].clone(), elements_array[2].clone());
+
+            println!("map ==> {:?}", map);
+
+            let _ = stream.write_all("+OK\r\n".as_bytes());
+        } else if elements_array[0].eq_ignore_ascii_case("get") {
+            if let Some(val) = map.get(&elements_array[1]) {
+                let value_to_send = format!("${}\r\n{}\r\n", val.as_bytes().len(), val);
+                let _ = stream.write_all(value_to_send.as_bytes());
+            } else {
+                println!("Key Doesn't exists");
+            }
         }
+
+        // use match expression here
     }
 
     stream
