069fef03053a885deb3347850c47f2cfa9fd63c0
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2025-12-09 12:08:12 +0530

    codecrafters submit [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -530,92 +530,11 @@ pub fn handle_xread(stream: &mut TcpStream, elements_array: &mut Vec<String>, st
         (map, _) = cvar.wait_timeout(map, Duration::from_millis(block_time as u64)).unwrap();
     }
 
-    let mut final_array_data_of_streams = format!("");
-    let mut no_of_valid_streams = 0;
+    // let mut final_array_data_of_streams = format!("");
+    // let mut no_of_valid_streams = 0;
 
-    let no_of_streams = (elements_array.len() - 2) / 2;
+    let (final_array_data_of_streams, no_of_valid_streams) = helper::get_streams_array(&map, elements_array);
 
-    for i in 0..no_of_streams {
-
-
-
-
-
-        
-        // will get these in u128
-        let (start_id_time, start_id_sequence, end_id_time, end_id_sequence) =
-            helper::get_start_and_end_indexes_for_xread(&elements_array[i + 2 + no_of_streams]);
-
-        let mut streams_array = format!("*2\r\n${}\r\n{}\r\n", &elements_array[i + 2].len(), &elements_array[i + 2]);
-
-        // getting the full entries array
-        // stream exists
-        println!("elemenst array {:?}", elements_array);
-
-
-        if let Some(value_entry) = map.get(&elements_array[i + 2]) {
-            match &value_entry.value {
-                types::StoredValue::Stream(entry_vector) => {
-                    let filtered_data: Vec<&types::Entry> = entry_vector
-                        .iter()
-                        .filter(|e| {
-                            let mut s = e.id.splitn(2, "-");
-                            let (iteration_id_time, iteration_id_seq) = (
-                                s.next().unwrap().parse::<u128>().unwrap(),
-                                s.next().unwrap().parse::<u128>().unwrap(),
-                            );
-                            iteration_id_time <= end_id_time &&
-                                iteration_id_time >= start_id_time &&
-                                iteration_id_seq >= start_id_sequence &&
-                                iteration_id_seq <= end_id_sequence
-                        })
-                        .collect();
-
-                    if filtered_data.len() == 0 {
-                        continue;
-                    }
-
-                    no_of_valid_streams += 1;
-
-                    // array of array data is all the data for a particular stream
-                    let mut array_of_array_data = format!("*{}\r\n", filtered_data.len());
-
-                    for entry in filtered_data {
-                        let entry_id = &entry.id;
-                        let hashmap = &entry.map;
-
-                        let mut entry_array = format!("*2\r\n");
-                        let mut hashmap_array = format!("*{}\r\n", hashmap.len() * 2);
-
-                        for (key, value) in hashmap {
-                            let map_bulk_data = format!(
-                                "${}\r\n{}\r\n${}\r\n{}\r\n",
-                                key.len(),
-                                key,
-                                value.len(),
-                                value
-                            );
-                            println!("key value {}, {}", key, value);
-                            hashmap_array.push_str(&map_bulk_data);
-                        }
-
-                        entry_array.push_str(&format!("${}\r\n{}\r\n", entry_id.len(), entry_id));
-                        entry_array.push_str(&hashmap_array);
-
-                        array_of_array_data.push_str(&entry_array);
-                    }
-
-                    streams_array.push_str(&array_of_array_data);
-                    final_array_data_of_streams.push_str(&streams_array);
-                }
-                _ => {}
-            }
-
-            // stream doesn't exists
-        } else {
-            let _ = stream.write_all(b"stream with given stream key doesn't exists");
-        }
-    }
 
     if no_of_valid_streams != 0 {
         let final_array_data_of_streams_head = format!("*{}\r\n", no_of_valid_streams);
